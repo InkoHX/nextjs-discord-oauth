47e5d0e6e96402e4d37a4d3447d81ad7bb5ac30e
InkoHX <me@inkohx.dev>
2021-11-09 00:16:00 +0900

    Revert "strict-dynamic"
    This reverts commit 55fcc5450ba2cf99fa494446f58a92ec40feae32.

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return defaultConfig
+  }
+
+  return {
+    ...defaultConfig,
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'",
+            },
+          ],
+        },
+      ]
+    },
+  }
+}

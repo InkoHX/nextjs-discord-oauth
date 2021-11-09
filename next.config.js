eff837f571da04b8b36bfe32bee08a64d9ee36bf
InkoHX <me@inkohx.dev>
2021-11-10 02:39:55 +0900

    format code

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ module.exports = (phase, { defaultConfig }) => {
           headers: [
             {
               key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; upgrade-insecure-requests",
+              value:
+                "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; upgrade-insecure-requests",
             },
           ],
         },
